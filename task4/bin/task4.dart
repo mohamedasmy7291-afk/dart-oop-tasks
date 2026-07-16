af3ad8be0ba621e091a180571551c1f70c62c154
mohamedasmy7291-afk <mohamedasmy7291@gmail.com>
2026-07-16 17:08:40 +0300

    Create task4.dart

---
+++ task4/bin/task4.dart
@@ -0,0 +1,21 @@
+class Rectangle {
+  double width;
+  double height;
+
+  Rectangle(this.width, this.height);
+
+  double area() {
+    return width * height;
+  }
+
+  double perimeter() {
+    return 2 * (width + height);
+  }
+}
+
+void main() {
+  Rectangle rectangle = Rectangle(5, 4);
+
+  print("Area: ${rectangle.area()}");
+  print("Perimeter: ${rectangle.perimeter()}");
+}
