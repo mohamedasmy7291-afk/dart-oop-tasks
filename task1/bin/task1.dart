b86310b5ab490d008d9b788cbb73d3ba8b657757
mohamedasmy7291-afk <mohamedasmy7291@gmail.com>
2026-07-16 16:59:44 +0300

    Create task1.dart

---
+++ task1/bin/task1.dart
@@ -0,0 +1,19 @@
+class Student {
+  String? name;
+  int? age;
+ String? grade;
+
+  Student(this.name, this.age, this.grade);
+
+  void displayInfo() {
+    print("Name: $name");
+    print("Age: $age");
+    print("Grade: $grade");
+  }
+}
+
+void main() {
+  Student student = Student("Mohamed", 17, "A");
+
+  student.displayInfo();
+}
