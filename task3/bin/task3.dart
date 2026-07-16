04cf202143020d991e1cf55cf42d32313c3c9f67
mohamedasmy7291-afk <mohamedasmy7291@gmail.com>
2026-07-16 17:07:46 +0300

    Create task3.dart

---
+++ task3/bin/task3.dart
@@ -0,0 +1,32 @@
+class BankAccount {
+  String ownerName;
+  double balance;
+
+  BankAccount(this.ownerName, this.balance);
+
+  void deposit(double amount) {
+    balance = balance + amount;
+    print("Deposit: $amount");
+  }
+
+  void withdraw(double amount) {
+    if (amount > balance) {
+      print("Insufficient balance");
+    } else {
+      balance = balance - amount;
+      print("Withdraw: $amount");
+    }
+  }
+
+  void showBalance() {
+    print("Balance: $balance");
+  }
+}
+
+void main() {
+  BankAccount account = BankAccount("Ali", 1000);
+
+  account.deposit(500);
+  account.withdraw(300);
+  account.showBalance();
+}
